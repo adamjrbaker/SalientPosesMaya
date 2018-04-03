a82a740d679ef508d0e64c0e23454d308c863756
Richard Roberts <rykardo.r@gmail.com>
2018-04-04 08:01:42 +1200

    Added MayaUtils (logging and type-checking for Maya)

--- src/MayaUtils.hpp
+++ src/MayaUtils.hpp
@@ -8,6 +8,49 @@
 #ifndef MayaUtils_hpp
 #define MayaUtils_hpp
 
-#include <stdio.h>
+#include <maya/MString.h>
+#include <maya/MObject.h>
+#include <maya/MStatus.h>
+
+class Log {
+public:
+    static void print(MString message);
+    static void warning(MString message);
+    static void error(MString message);
+    
+    static void print(std::string message) { print(MString(message.c_str())); }
+    static void warning(std::string message) { warning(MString(message.c_str())); }
+    static void error(std::string message) { error(MString(message.c_str())); }
+    
+    static void print(const char* message) { print(MString(message)); }
+    static void warning(const char* message) { warning(MString(message)); }
+    static void error(const char* message) { error(MString(message)); }
+    
+    static void showStatus(MStatus status, std::string message);
+    static void showStatusWhenError(MStatus status, std::string message);
+    static void showStatus(MStatus status);
+    static void showStatusWhenError(MStatus status);
+    
+    template <typename T>
+    static void arrayAsMatrix(T *data, int nCols, int nRows) {
+        std::ostringstream os;
+        for (int i = 0; i < nCols; i++) {
+            os << i << " = [";
+            for (int j = 0; j < nRows; j++) {
+                int index = i * nRows + j;
+                os  << data[index] << ",";
+            }
+            os << "]\n";
+        }
+        Log::print(MString(os.str().c_str()));
+    }
+};
+
+class MayaCheck {
+public:
+    static void objectIsPointArray(MObject obj);
+    static void objectIsFloatArray(MObject obj);
+};
+
 
 #endif /* MayaUtils_hpp */
